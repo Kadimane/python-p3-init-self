c304dbd7dca696b19cfc8e3968417160c9d8868e
Kadimane <kadimanegloriah@gmail.com>
2023-05-18 08:28:21 +0300

    complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+dog1 = Dog("Fido", "Dalmatian")
+print(dog1.name)   
+print(dog1.breed)  
+
+dog2 = Dog("Max")
+print(dog2.name)   
+print(dog2.breed)  
+
+
+
+
+pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+guido = Person("Guido")
+print("Guido".name)  
+
+person2 = Person("Emma")
+print("Emma".name)  
+
+pass
